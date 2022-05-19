b2bc0b2080b5fb451a4dc21ee0586caa4ee73206
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-05-19 14:36:04 +0300

    feat: complete 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const WAY = path.join(__dirname, 'text.txt');
+const HELLOYMESSAGE = 'Hi, write anything!!';
+const BYMESSAGE = 'Bye, thank for interaction!!';
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.prompt();
+console.log(HELLOYMESSAGE);
+
+rl.on('line', (line) => {
+    switch (line.trim()) {
+        case 'exit':
+            console.log(BYMESSAGE);
+            process.exit(0);
+        default:
+            const data = line + '\n';
+            fs.appendFile(WAY, data, (err) => {
+                if (err) throw err;
+                console.log('ok, write more');
+            });
+            break;
+    }
+    rl.prompt();
+}).on('close', () => {
+    console.log(BYMESSAGE);
+    process.exit(0);
+});
