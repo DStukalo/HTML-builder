4d8fb58089fd2887e81d420ce10b8f8eb96e466d
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-05-20 14:16:22 +0300

    feat: update 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const fs = require('fs');
+const WAY = path.join(__dirname, 'secret-folder');
 
-fs.readdir(__dirname, function (err, items) {
-    console.log(items);
+fs.readdir(WAY, function (err, items) {
+    if (err) {
+        throw err;
+    }
 
-    for (var i = 0; i < items.length; i++) {
-        if (path.extname(items[i]) !== '') {
-            console.log(items[i]);
-        }
+    for (let i = 0; i < items.length; i++) {
+        let cur = path.join(WAY, items[i]);
+        fs.stat(cur, (err, stats) => {
+            if (stats.isFile()) {
+                console.log(
+                    `${path.parse(items[i]).name} - ${path
+                        .parse(items[i])
+                        .ext.slice(1)} - ${stats.size}b`
+                );
+            }
+        });
     }
 });
