20b001b0971e498a0532c5c0fd057ca689b1275a
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-05-20 19:27:03 +0300

    feat: complete 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const path = require('path');
+const fs = require('fs');
 
+const wayCreateDir = path.join(__dirname, 'files-copy');
+const wayPrevDir = path.join(__dirname, 'files');
+
+fs.mkdir(wayCreateDir, { recursive: true }, (err) => {
+    if (err) throw err;
+});
+fs.readdir(wayPrevDir, function (err, items) {
+    if (err) throw err;
+    for (let i = 0; i < items.length; i++) {
+        let prev = path.join(wayPrevDir, items[i]);
+        let next = path.join(wayCreateDir, items[i]);
+        fs.copyFile(prev, next, (err) => {
+            if (err) throw err;
+        });
+    }
+});
+fs.readdir(wayCreateDir, function (err, items) {
+    if (err) throw err;
+    for (let i = 0; i < items.length; i++) {
+        let curItem = path.join(wayCreateDir, items[i]);
+        let itemInPrev = path.join(wayPrevDir, items[i]);
+        fs.access(itemInPrev, (err) => {
+            console.log(`${itemInPrev} ${err ? 'does not exist' : 'exists'}`);
+            if (err) {
+                fs.unlink(curItem, (err) => {
+                    if (err) throw err;
+                    console.log('curItem was deleted');
+                });
+            }
+        });
+    }
+});
